b15c48173febdbcbb00210787fca3b10f816a6e2
Sreevidya B <86249075+Sreevidya181@users.noreply.github.com>
2022-09-17 13:13:49 +0530

    Add files via upload

---
+++ playTune.py
@@ -0,0 +1,7 @@
+import random
+from playsound import playsound
+d = random.randint(1,2)
+if d==1:
+    playsound("Play Sound\Music\Weekend.mp3")
+else:
+    playsound("Play Sound\Music\mixkit-raising-me-higher-34.mp3")
